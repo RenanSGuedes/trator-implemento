aefb6c82694a15703eb0efa3f74d423ff81c821f
RenanSGuedes <renanguedes.br@gmail.com>
2020-10-19 14:23:59 -0300

    :apple:

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from math import degrees as deg, radians as rad
 
 d = float(input("Profundide da ferramenta (m): "))
 w = float(input("Largura da ferramenta (m): "))
-delta = float(input("Valor de delta (em graus): "))
-phi = float(input("Valor de phi (em graus): "))
-c = float(input("Valor de c (em Pa): "))
-ca = float(input("Valor de ca (em Pa): "))
-gamma = float(input("Peso específico (gamma, em N/m³): "))
 
 # Primeiro passo: Tipo de ferramenta
 
 if d/w < 1:
-  print("Blade")
+  print("----------------------------------------------")
+  print("Tipo de ferramenta: Blade (espessa => d/w < 1)")
+  print("----------------------------------------------")
   dc = d
 elif (d/w >= 1 and d/w < 6):
-  print("Narrow tine")
+  print("---------------------------------------------------------")
+  print("Tipo de ferramenta: Narrow tine (Estreita => 1 < d/w < 6)")
+  print("---------------------------------------------------------")
   dc = d
 else:
-  print("Very narrow tine")
-  dc = d
+  print("----------------------------------------------------------------")
+  print("Tipo de ferramenta: Very narrow tine (Muito estreita => d/w > 6)")
+  print("----------------------------------------------------------------")
+  dc = w + .6 * d
 
 # Segundo passo: Acha o valor de beta crítico
 alpha = float(input("Ângulo de ataque (alpha, em graus): "))
@@ -27,18 +28,40 @@ m = 4.0704 - (0.0940983 * alpha) + (0.00149667 * alpha**2) - (0.0000121924 * alp
 
 beta = deg(atan(1/(m - (tan(rad(alpha)))**(-1))))
 
+print("---------------------------------------------")
+print("alpha = {:.2f} => m = {:.2f}".format(alpha, m))
+print("beta crítico = {:.2f}°".format(beta))
+print("---------------------------------------------")
+
 # Terceiro passo: Obter os adimensionais
 
-r = d * ((tan(rad(alpha)))**(-1) + (tan(rad(beta)))**(-1))
+print("Cálculo dos adimensionais")
+print("---------------------------------------------")
+
+delta = float(input("Valor de delta (em graus): "))
+phi = float(input("Valor de phi (em graus): "))
 b = float(input(("Valor de b (em metros): ")))
 rho = float(input("Valor de rho (em graus): "))
+r = d * ((tan(rad(alpha)))**(-1) + (tan(rad(beta)))**(-1))
 
 ngamma = (r/(2*d) * (1 + (2 * r)/(3 * b) * sin(rad(rho))))/(cos(rad(alpha + delta)) + sin(rad(alpha + delta)) * (tan(rad(beta + phi)))**(-1))
-
+nq = ((r/d) * (1 + (r/b) * sin(rad(rho))))/(cos(rad(alpha + delta)) + sin(rad(alpha + delta)) * (tan(rad(beta + phi)))**(-1))
+nca = (1 - (tan(rad(alpha)))**(-1) * (tan(rad(beta + phi)))**(-1))/(cos(rad(alpha + delta)) + sin(rad(alpha + delta)) * (tan(rad(beta + phi)))**(-1))
 nc = (1 + tan(rad(beta))**(-1) * tan(rad(beta + phi))**(-1) * (1 + (r/b) * sin(rad(rho))))/(cos(rad(alpha + delta)) + sin(rad(alpha + delta)) * tan(rad(beta + phi))**(-1))
 
+print("-------------------------------------------------")
+print("ngamma = {:.2f}".format(ngamma))
+print("nq = {:.2f}".format(nq))
+print("nca = {:.2f}".format(nca))
+print("nc = {:.2f}".format(nc))
+print("-------------------------------------------------")
+
 q = float(input("Valor do carregamento (q, em Pascal): "))
+c = float(input("Valor de c (em Pa): "))
+ca = float(input("Valor de ca (em Pa): "))
+gamma = float(input("Peso específico (gamma, em N/m³): "))
 
-h = (gamma * dc**(2) * ngamma + c * dc * nc + q * dc * 0) * (w + d * (m - (m - 1)/3)) * sin(rad(alpha + delta))
+print("-------------------------------------------------")
+h = (gamma * dc**(2) * ngamma + c * dc * nc + q * dc * nq) * (w + d * (m - (m - 1)/3)) * sin(rad(alpha + delta))
 
-print(m, beta, r, ngamma, nc, h)
+print("H = {:.2f} N".format(h))
